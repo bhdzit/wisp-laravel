ff71f77f58a22a6519476002676adbc895fd4529
bhdz <bhdzit97@gmail.com>
2020-07-31 14:06:46 -0500

    Creccion de extras

--- app/Http/Controllers/PagosController.php
+++ app/Http/Controllers/PagosController.php
@@ -75,7 +75,8 @@ date_default_timezone_set('America/Mexico_City');
       $pagos->append($pago);
       $customPaper = array(0,0,567.00,150);
       $credenciales=DB::select("select * from wisp_emby where wem_id=?",[$pago->wps_servicios]);
-      $pdf = PDF::loadView('layouts.dompdf',compact('request',['pagos','credenciales']))->setPaper($customPaper, 'landscape');
+      $extras=null;
+      $pdf = PDF::loadView('layouts.dompdf',compact('request',['pagos','extras','credenciales']))->setPaper($customPaper, 'landscape');
   //    $pdf->save('my_stored_file.pdf')->stream('download.pdf');
       return  $pdf->stream();#redirect()->route('pagos.index'); view('layouts.dompdf');
 
